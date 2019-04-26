d35c6a756a8066393dd664ca6752efbf96bd290b
buoyantair <buoyantair@protonmail.com>
2019-04-26 10:06:03 +0530

    implement cursor based pagination

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -1,12 +1,23 @@
 import { combineResolvers } from 'graphql-resolvers'
 import { isAuthenticated, isMessageOwner } from './authorization'
+import * as Sequelize from 'sequelize';
 
 export default {
   Query: {
-    messages: async (parent, { offset = 0, limit = 100 }, { models }) => {
+    messages: async (parent, { cursor, limit = 100 }, { models }) => {
+      const cursorOptions = cursor
+        ? {
+          where: {
+            createdAt: {
+              [Sequelize.Op.lt]: cursor,
+            },
+          },
+        }
+        : {};
       return models.Message.findAll({
-        offset,
-        limit
+        order: [['createdAt', 'DESC']],
+        limit,
+        ...cursorOptions
       })
     },
     message: async (parent, { id }, { models }) => {
