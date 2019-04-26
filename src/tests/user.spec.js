fe26da0753e5fad79c8730ccbcb152298437b7de
buoyantair <buoyantair@protonmail.com>
2019-04-26 12:05:52 +0530

    implement e2e tests

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,5 +1,58 @@
 import { expect } from "chai";
+import * as userApi from './api'
 
 describe('users', () => {
-  expect('user').to.eql('user')
+  describe('user(id: String!): User', () => {
+    it('returns a user when user can be found', async () => {
+      const expectedResult = {
+        data: {
+          user: {
+            id: '1',
+            username: 'Alice',
+            email: 'alice@alice.com',
+            role: 'ADMIN',
+          }
+        }
+      }
+
+      const result = await userApi.user({ id: '1' })
+
+      expect(result.data).to.eql(expectedResult)
+    })
+
+    it('returns null when user cannot be found', async () => {
+      const expectedResult = {
+        data: {
+          user: null
+        }
+      }
+
+      const result = await userApi.user({ id: '42' })
+
+      expect(result.data).to.eql(expectedResult)
+    })
+  })
+
+  describe('deleteUser(id: ID!): Boolean!', () => {
+    it('returns an error because only admins can delete a user', async () => {
+      const {
+        data: {
+          data: {
+            signIn: { token }
+          }
+        }
+      } = await userApi.signIn({
+        login: 'Louis',
+        password: 'louis9393'
+      })
+
+      const {
+        data: {
+          errors
+        }
+      } = await userApi.deleteUser({ id: '1' }, token)
+
+      expect(errors[0].message).to.eql('Not authorized as admin')
+    })
+  })
 })
